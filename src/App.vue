3e3cfbf4e0f07b15573c0369b78c825c7d184843
Achsuthan <achsuthan@icloud.com>
2020-02-23 16:47:34 +0530

    1. Css added 2. Dropdown logic added
    >>> ach

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <br />
+    <div class="row">
+      <div class="col-12">
+        <span>Single Select :</span>
+        <VueDropDown
+          class="col-12"
+          :list="list"
+          :isSingle="true"
+          :selectedList="singleSelectedList"
+          @inputChange="inputChangeFn"
+          @selectedValues="val => selectedValeChangedFn(val,true)"
+        />
+        <span>Selected Values :</span>
+        <br />
+        <span>{{singleSelectedList.length > 0 ? '1 : ' : ''}}  {{singleSelectedList.length > 0 ? singleSelectedList[0].name : ''}}</span>
+        <br />
+        <br />
+        <br />
+
+        <span>Multi Select :</span>
+        <VueDropDown
+          class="col-12"
+          :list="list"
+          :isSingle="false"
+          :selectedList="multiSelectedList"
+          @inputChange="inputChangeFn"
+          @selectedValues="val=> selectedValeChangedFn(val, false)"
+        />
+        <span>Selected Values :</span>
+        <template v-for="(val, index) in multiSelectedList">
+          <div :key="index">
+            <span>{{index + 1}} : {{val.name}}</span>
+            <br />
+          </div>
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueDropDown from "./components/VueDropDown";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    VueDropDown
+  },
+  data() {
+    return {
+      singleSelectedList: [],
+      multiSelectedList: [],
+      list: [
+        {
+          name: "End Game",
+          value: "End Game1"
+        },
+        {
+          name: "The Lion King",
+          value: "The Lion King"
+        },
+        {
+          name: "Spider-Man: Far From Home",
+          value: "Spider-Man: Far From Home"
+        },
+        {
+          name: "Captain Marvel",
+          value: "Captain Marvel"
+        },
+        {
+          name: "Jorker",
+          value: "Jorker"
+        }
+      ]
+    };
+  },
+  methods: {
+    inputChangeFn(val) {
+      console.log("value chaned ", val);
+    },
+    selectedValeChangedFn(values, isSingle) {
+      isSingle
+        ? (this.singleSelectedList = values)
+        : (this.multiSelectedList = values);
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/VueDropDown.vue
@@ -0,0 +1,173 @@
+<template>
+  <div>
+    <div
+      class="dd-header"
+      @click="toggleListFn()"
+      @mouseenter="mouseEnterEventFn()"
+      @mouseleave="mouseLeaveEventFn()"
+    >
+      <label class="col-12">
+        <ul style="display: inline">
+          <template v-for="(item, index) in internalSelectedList">
+            <li :key="index" @click="reveItemFn(index)" style="display: inline">
+              <div
+                style="display: inline-block; padding: 2px; border: 1px solid #0074D9; background-color: #0074D9; border-radius : 5px; margin: 5px;cursor: pointer; color: white"
+              >
+                {{item[selectedlabelName]}}
+                <span style="paddingLeft: '5px', color: 'white'">X</span>
+              </div>
+            </li>
+          </template>
+
+          <input
+            class="form-control"
+            style="outline: none; border: none; font-size: 14px;"
+            :placeholder="placeholder"
+            v-model="value"
+          />
+        </ul>
+      </label>
+    </div>
+    <template v-if="listOpen">
+      <div>
+        <ul @mouseenter="mouseEnterEventFn()" @mouseleave="mouseLeaveEventFn()" class="dd-list">
+          <template v-for="(item, index) in list">
+            <li
+              :class="selectedClassnameFn(item) ? 'dd-list-item selected' : 'dd-list-item' "
+              :key="index"
+              @click="dropDownSelectedItemFn(item);"
+            >{{item[labelName]}}</li>
+          </template>
+        </ul>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueDropDown",
+  props: {
+    placeholder: {
+      default: "Search...",
+      type: String
+    },
+    labelName: {
+      default: "name",
+      type: String
+    },
+    selectedlabelName: {
+      default: "name",
+      type: String
+    },
+    isSingle: {
+      default: true,
+      type: Boolean
+    },
+    list: {
+      default: function() {
+        return [];
+      },
+      type: Array
+    },
+    selectedList: {
+      default: function() {
+        return [];
+      },
+      type: Array
+    },
+    searchKey: {
+      default: "",
+      type: String
+    }
+  },
+  data() {
+    return {
+      value: "",
+      listOpen: false,
+      internalSelectedList: []
+    };
+  },
+  created() {
+    this.internalSelectedList = this.selectedList;
+    this.value = this.searchKey;
+  },
+  watch: {
+    selectedList(val) {
+      this.internalSelectedList = val;
+    },
+    searchKey(val) {
+      this.val = val;
+    },
+    value(val) {
+      this.$emit("inputChange", val.toUpperCase());
+    }
+  },
+  methods: {
+    toggleListFn() {
+      this.listOpen = !this.listOpen;
+    },
+    mouseEnterEventFn() {
+      window.removeEventListener("mousedown", close);
+    },
+    mouseLeaveEventFn() {
+      window.addEventListener("mousedown", close);
+    },
+    reveItemFn(index) {
+      let tmpArray = this.internalSelectedList.filter((item, i) => i !== index);
+      this.internalSelectedList = tmpArray;
+      this.$emit("selectedValues", tmpArray)
+    },
+    dropDownSelectedItemFn(item) {
+      // console.log("dropDownSelectedItemFn called", item);
+      let totalSelectedItem = [...this.internalSelectedList];
+      if (this.isSingle) {
+        totalSelectedItem = [item];
+        this.internalSelectedList = totalSelectedItem;
+      } else if (!this.selectedClassnameFn(item)) {
+        totalSelectedItem.push(item);
+        this.internalSelectedList = totalSelectedItem;
+      } else {
+        totalSelectedItem.splice(totalSelectedItem.indexOf(item), 1);
+        this.internalSelectedList = totalSelectedItem;
+      }
+      this.value = "";
+      this.$emit("selectedValues", totalSelectedItem)
+    },
+    selectedClassnameFn(item) {
+      let found = false;
+      this.internalSelectedList.filter(val => {
+        val[this.labelName].toUpperCase() === item[this.labelName].toUpperCase()
+          ? (found = true)
+          : null;
+      });
+      return found;
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import "./styles/global.css";
+.container {
+  padding: "2px";
+  margin: "5px";
+}
+
+.item {
+  color: "white";
+  display: "inline-block";
+  padding: "2px";
+  border: "1px solid #0074D9";
+  background-color: "#0074D9";
+  border-radius: "5px";
+  margin: "5px";
+  cursor: "pointer";
+}
+
+.inputField {
+  outline: "none";
+  border: "none";
+  font-size: "12px";
+}
+</style>
